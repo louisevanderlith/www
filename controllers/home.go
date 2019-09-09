0bba4ab584140373f232c415dee28e9b937884cd
Louise van der Lith <louisevanderlith@gmail.com>
2019-09-09 22:18:53 +0200

    Fixing pages

--- controllers/home.go
+++ controllers/home.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/louisevanderlith/droxolite/bodies"
 	"github.com/louisevanderlith/droxolite/context"
 	"github.com/louisevanderlith/droxolite/do"
 )
@@ -24,8 +23,6 @@ func (c *Home) Get(ctx context.Requester) (int, interface{}) {
 	}
 
 	log.Println(result)
-	//c.Setup("default", "Home", true)
-	//c.CreateTopMenu(getHomeMenu())
 
 	return http.StatusOK, result
 }
@@ -40,25 +37,5 @@ func (c *Home) GetSite(ctx context.Requester) (int, interface{}) {
 		return code, err
 	}
 
-	//pageTitle := "Home"
-	//dataObj, ok := result["Data"].(map[string]interface{})
-
-	/*if ok {
-		pageTitle = dataObj["Title"].(string)
-	}*/
-
-	//c.Setup("default", pageTitle, true)
-	//c.CreateTopMenu(getHomeMenu())
 	return http.StatusOK, result
-}
-
-func getHomeMenu() bodies.Menu {
-	result := bodies.NewMenu()
-
-	//result.AddItem("#portfolio", "Portfolio", "home fa fa-star", nil)
-	//result.AddItem("#aboutus", "About Us", "home fa fa-info", nil)
-	//result.AddItem("#contact", "Contact", "home fa fa-phone", nil)
-	//result.AddItem("/blogs/A10", "Blog", "home fa fa-phone", nil)
-
-	return *result
 }
