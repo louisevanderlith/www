6fe017c08db855f4550b5231eb3741199e3363d8
Louise van der Lith <louisevanderlith@gmail.com>
2020-07-06 20:30:50 +0200

    Updated Libs

--- web/index.entry.dart
+++ web/index.entry.dart
@@ -1,5 +1,17 @@
+import 'dart:html';
+
 import 'package:WWW.APP/contactform.dart';
 
 void main() {
   new ContactForm("#frmContact", "#btnContactSubmit");
+
+  window.onScroll.listen((event) { print("Sroll Happened"); scrollFunction();});
+}
+
+void scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("toTop").style.display = "block";
+  } else {
+    document.getElementById("toTop").style.display = "none";
+  }
 }
